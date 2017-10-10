9444f005719030b2caf39563eb7f6b36063842ad
Soar360 <Soar360@live.com>
2017-10-10 23:46:02 +0800

    RirStatsPublisher

---
+++ src/IP2Country/Publish/RirStatsPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Abp.Dependency;
+using Abp.Events.Bus.Handlers;
+using IP2Country.Dto;
+using IP2Country.Events;
+
+namespace IP2Country.Publish
+{
+    public class RirStatsPublisher : IEventHandler<RirStatsSourceUpdatedEventData>, ITransientDependency
+    {
+        private readonly List<IRirStatsSource> _rirStatsSources;
+
+        public RirStatsPublisher(IIocResolver iocResolver)
+        {
+            _rirStatsSources = iocResolver.ResolveAll<IRirStatsSource>().ToList();
+        }
+
+        public void HandleEvent(RirStatsSourceUpdatedEventData eventData)
+        {
+            var items = new List<RirStatsListDto>();
+            foreach (var source in _rirStatsSources)
+            {
+                items.AddRange(source.GetRirStats());
+            }
+        }
+    }
+}
