546afd748a5bbd9cec6ebf3974e79d4bc273b787
Soar360 <Soar360@live.com>
2017-10-10 22:58:54 +0800

    IP2Country.Net

---
+++ src/IP2Country/Net/IPAddressComparer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace IP2Country.Net
+{
+    public class IPAddressComparer : IComparer<IPAddress>
+    {
+        public int Compare(IPAddress x, IPAddress y)
+        {
+            if (x == null)
+            {
+                return -1;
+            }
+            if (y == null)
+            {
+                return 1;
+            }
+            var result = x.AddressFamily.CompareTo(y.AddressFamily);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            var xBytes = x.GetAddressBytes();
+            var yBytes = y.GetAddressBytes();
+
+            var octets = Math.Min(xBytes.Length, yBytes.Length);
+            for (var i = 0; i < octets; i++)
+            {
+                var octetResult = xBytes[i].CompareTo(yBytes[i]);
+                if (octetResult != 0)
+                {
+                    return octetResult;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ src/IP2Country/Net/IPAddressExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IP2Country.Net
+{
+    public static class IPAddressExtensions
+    {
+        public static IPAddress GetPreviousIPAddress(this IPAddress ipAddress)
+        {
+            return ipAddress.Add(-1);
+        }
+
+        public static string GetSortKey(this IPAddress ipAddress)
+        {
+            var arr = new List<string> {ipAddress.AddressFamily.ToString()};
+            foreach (var b in ipAddress.GetAddressBytes())
+            {
+                arr.Add(b.ToString("000"));
+            }
+            return string.Join(".", arr);
+        }
+
+        public static long GetCode(this IPAddress ipAddress)
+        {
+            if (ipAddress.AddressFamily != AddressFamily.InterNetwork)
+            {
+                throw new ArgumentOutOfRangeException(nameof(ipAddress));
+            }
+            var data = ipAddress.GetAddressBytes();
+            Array.Reverse(data);
+            return BitConverter.ToUInt32(data, 0);
+        }
+
+        public static IPAddress Add(this IPAddress ipAddress, int total)
+        {
+            var code = ipAddress.GetCode() + total;
+            var data = BitConverter.GetBytes(Convert.ToUInt32(code));
+            Array.Reverse(data);
+            return new IPAddress(data);
+        }
+    }
+}
