91613fb0b17bf7dc16020f0a1c79065beb628ce1
Soar360 <Soar360@live.com>
2017-10-10 22:26:12 +0800

    add Abp and Topshelf.

---
+++ src/IP2Country/IP2CountryModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Abp.Castle.Logging.Log4Net;
+using Abp.Modules;
+
+namespace IP2Country
+{
+    [DependsOn(typeof(AbpCastleLog4NetModule))]
+    public class IP2CountryModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ src/IP2Country/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Abp;
+using Abp.Castle.Logging.Log4Net;
+using Abp.PlugIns;
+using Castle.Facilities.Logging;
+using Topshelf;
+
+namespace IP2Country
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var log4NetConfigFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log4net.config");
+            HostFactory.Run(
+                cfg =>
+                {
+                    cfg.UseLog4Net(log4NetConfigFile);
+                    cfg.Service<AbpBootstrapper>(config =>
+                    {
+                        config.ConstructUsing(AbpBootstrapper.Create<IP2CountryModule>);
+                        config.WhenStarted(bootstrapper =>
+                        {
+                            bootstrapper.PlugInSources.AddFolder(AppDomain.CurrentDomain.BaseDirectory);
+                            bootstrapper
+                                .IocManager
+                                .IocContainer
+                                .AddFacility<LoggingFacility>(
+                                    loggingFacility => loggingFacility.UseAbpLog4Net()
+                                        .WithConfig(log4NetConfigFile)
+                                );
+                            bootstrapper.Initialize();
+                        });
+                        config.WhenStopped(bootstrapper => bootstrapper.Dispose());
+                    });
+
+                    cfg.RunAsLocalSystem();
+                    cfg.SetServiceName(typeof(Program).Namespace);
+                    cfg.SetDisplayName(typeof(Program).Namespace);
+                }
+            );
+        }
+    }
+}
