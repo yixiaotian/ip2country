a15f6d14fdf2099f194e544cacf33be6983b8dfa
Soar360 <Soar360@live.com>
2017-10-10 23:58:35 +0800

    IRirStatsDataExporter

---
+++ src/IP2Country/Publish/IRirStatsDataExporter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using IP2Country.Dto;
+
+namespace IP2Country.Publish
+{
+    public interface IIP2CountryDataExporter
+    {
+        string Export(IReadOnlyCollection<RirStatsListDto> items);
+    }
+}
